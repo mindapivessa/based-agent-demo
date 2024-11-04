1bf5b0e6e951dde19b8625209ce50fdf6d6fe427
Mind Apivessa <mindapi@Minds-MBP.fios-router.home>
2024-11-04 13:04:12 -0500

    use token SVG

--- app/page.tsx
+++ app/page.tsx
@@ -236,7 +236,7 @@ export default function Component() {
       case 'request_faucet_funds': return <RequestSvg />
       case 'get_balance': return <WalletSvg />
       case 'swap_token': return <SwapSvg />
-      case 'transfer_token': return <SendSvg />
+      case 'transfer_token': return <TokenSvg />
       case 'transfer_nft': return <NftSvg />
       case 'user': return <SendSvg />
       default: return null
